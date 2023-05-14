7ef668a90fc2ccc26aa0e0cd8f112ee1c0aea99c
AvasBrahma <avasbrahma@gmail.com>
2023-05-14 11:40:15 +0530

    modified and commented unused function in cartItem.js

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,27 +10,31 @@ class CartItem extends React.Component{
            qty: 1,
            img: ''
         }
-
+ 
     }
+
      increaseQuantity =()=>{
-        // console.log('Test');
-        // console.log('this', this.state);
-
-        //setState() Form 1
-    //     this.setState({
-    //         qty: this.state.qty+1
-    //     });
-
-
-    //  setstate form 2
        this.setState((prevState)=>{
         return {
             qty: prevState.qty+1
         }
        })
      }
-    render(){
 
+     decreaseQuantity=()=>{
+        const { qty } =this.state;
+        if(qty==0){
+            return;
+        }
+        this.setState((prevState)=>{
+            return{
+                qty: prevState.qty-1
+            }
+        })
+
+     }
+    render(){
+  
         const { price, title, qty }= this.state;
         return (
             
@@ -51,7 +55,9 @@ class CartItem extends React.Component{
                     src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
                     onClick= {this.increaseQuantity} 
                     />
-                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png"  />
+                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png"  
+                    onClick={this.decreaseQuantity}
+                    />
                     <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"  />
        
                  </div>
